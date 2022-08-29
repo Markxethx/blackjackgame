ea670ca7744456e7492a2824b9e4055652ecf7b9
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2022-08-29 22:48:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,70 @@
-messageEl = document.getElementById('message-el')
-cardsEl = document.getElementById('cards-el')
-sumEl = document.getElementById('sum-el')
-let firstCards = getRandomCard()
-let secondCards = getRandomCard()
-let cards = [firstCards , secondCards]
-let sum = firstCards + secondCards
-let message = ""
-
-function getRandomCard(){
-    let randomNumer = Math.floor(Math.random()*13) + 1
-    if (randomNumer > 10){
-        return 10
-    }   else if (randomNumer === 1 ){
-        return 11
-    } else {
-        return randomNumer
-    }
-}
-
-function startGame(){
-    renderGame()
-}
-
-function renderGame(){
-    cardsEl.textContent = "Cards:"
-    for (let i = 0; i < cards.length; i++){
-        cardsEl.textContent += cards[i] + " "
-    }
-
-    sumEl.textContent = "Sum: " + sum   
-    if (sum <= 20){
-        message = "Do you wish to draw a new Card?"
-    } else if (sum === 21){
-        message = "You got a BlackJack"
-    } else{
-        message = "You are out of the game"
-    }
-    messageEl.textContent = message
-}
-
-function newCard(){
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    renderGame()
+let chooseCard1 = document.getElementById('box-card-1')
+let chooseCard2 = document.getElementById('box-card-2')
+let chooseCard3 = document.getElementById('box-card-3')
+let chooseCard4 = document.getElementById('box-card-4')
+let chooseCard5 = document.getElementById('box-card-5')
+let chooseCard6 = document.getElementById('box-card-6')
+let chooseCard7 = document.getElementById('box-card-7')
+let chooseCard8 = document.getElementById('box-card-8')
+let chooseCard9 = document.getElementById('box-card-9')
+let chooseCard10 = document.getElementById('box-card-10')
+let chooseCard11 = document.getElementById('box-card-11')
+let chooseCard12 = document.getElementById('box-card-12')
+let chooseCard13 = document.getElementById('box-card-13')
+cards = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸"]
+function card1(){
+    randomEmoji = Math.floor(Math.random() * 13) + 1
+    chooseCard1.textContent = cards[randomEmoji]
 
 }
+function card2(){
+    randomEmoji = Math.floor(Math.random() * 13) + 1
+    chooseCard2.textContent = cards[randomEmoji]
+}
+function card3(){
+    randomEmoji = Math.floor(Math.random() * 13) + 1
+    chooseCard3.textContent = cards[randomEmoji]
+}
+function card4(){
+    randomEmoji = Math.floor(Math.random() * 13) + 1
+    chooseCard4.textContent = cards[randomEmoji]
+}
+function card5(){
+    randomEmoji = Math.floor(Math.random() * 13) + 1
+    chooseCard5.textContent = cards[randomEmoji]
+}
+function card6(){
+    randomEmoji = Math.floor(Math.random() * 13) + 1
+    chooseCard6.textContent = cards[randomEmoji]
+}
+function card7(){
+    randomEmoji = Math.floor(Math.random() * 13) + 1
+    chooseCard7.textContent = cards[randomEmoji]
+}
+function card8(){
+    randomEmoji = Math.floor(Math.random() * 13) + 1
+    chooseCard8.textContent = cards[randomEmoji]
+}
+function card9(){
+    randomEmoji = Math.floor(Math.random() * 13) + 1
+    chooseCard9.textContent = cards[randomEmoji]
+}
+function card10(){
+    randomEmoji = Math.floor(Math.random() * 13) + 1
+    chooseCard10.textContent = cards[randomEmoji]
+}
+function card11(){
+    randomEmoji = Math.floor(Math.random() * 13) + 1
+    chooseCard11.textContent = cards[randomEmoji]
+}
+function card12(){
+    randomEmoji = Math.floor(Math.random() * 13) + 1
+    chooseCard12.textContent = cards[randomEmoji]
+}
+function card13(){
+    randomEmoji = Math.floor(Math.random() * 13) + 1
+    chooseCard13.textContent = cards[randomEmoji]
+}
 
+    
+    
